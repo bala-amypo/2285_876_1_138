ac2a63ccbe027b50bb663b002fc3929ffa2c235e
bala-amypo <bala@amypo.in>
2025-12-22 07:45:44 +0000

    User commit

--- src/main/java/com/example/demo/servlet/HelloServlet.java
+++ src/main/java/com/example/demo/servlet/HelloServlet.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(urlPatterns = "/hello-servlet")
+@WebServlet(urlPatterns = "9057.pro604cr.amypo.ai")
 public class HelloServlet extends HttpServlet {
     private String message;
 
