af90e60452d5233b3a176635c6ca32ab5bc2ca78
bala-amypo <bala@amypo.in>
2025-12-25 18:54:08 +0000

    User commit

---
+++ src/main/java/com/example/demo/security/CustomerDetails.java
@@ -0,0 +1,56 @@
+package com.example.demo.security;
+
+import com.example.demo.model.Guest;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+
+public class CustomUserDetails implements UserDetails {
+
+    private final Guest guest;
+
+    public CustomUserDetails(Guest guest) {
+        this.guest = guest;
+    }
+
+    public Long getId() {
+        return guest.getId();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public String getPassword() {
+        return guest.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return guest.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
