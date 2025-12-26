aed6137d69e745d2c73987bbfe40e03386a3a7aa
bala-amypo <bala@amypo.in>
2025-12-26 08:37:38 +0000

    User commit

--- src/main/java/com/example/demo/servlet/HelloServlet.java
+++ src/main/java/com/example/demo/servlet/HelloServlet.java
@@ -1,19 +1,17 @@
 package com.example.demo.servlet;
 
-import jakarta.servlet.ServletConfig;
-import jakarta.servlet.ServletException;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
 
-@WebServlet(urlPatterns = "/hello-servlet")
+@WebServlet("/hello-servlet")
 public class HelloServlet extends HttpServlet {
 
     private String message;
 
     @Override
-    public void init(ServletConfig config) throws ServletException {
-        super.init(config);
-        this.message = "Hello Tomcat";
+    public void init(ServletConfig config) {
+        message = "Hello Tomcat";
     }
 
     public String getMessage() {
