b0fde54c22237f93cf60568009be2ff14569bd7f
bala-amypo <bala@amypo.in>
2025-12-26 07:58:37 +0000

    User commit

--- src/main/java/com/example/demo/servlet/HelloServlet.java
+++ src/main/java/com/example/demo/servlet/HelloServlet.java
@@ -1,32 +1,32 @@
-// package com.example.demo.servlet;
+package com.example.demo.servlet;
 
-// import javax.servlet.ServletConfig;
-// import javax.servlet.ServletException;
-// import javax.servlet.annotation.WebServlet;
-// import javax.servlet.http.HttpServlet;
-// import javax.servlet.http.HttpServletRequest;
-// import javax.servlet.http.HttpServletResponse;
-// import java.io.IOException;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
-// @WebServlet(urlPatterns = "/hello-servlet")
-// public class HelloServlet extends HttpServlet {
-//     private String message;
+@WebServlet(urlPatterns = "/hello-servlet")
+public class HelloServlet extends HttpServlet {
+    private String message;
 
-//     @Override
-//     public void init(ServletConfig config) throws ServletException {
-//         super.init(config);
-//         this.message = "Hello Tomcat";
-//     }
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+        this.message = "Hello Tomcat";
+    }
 
-//     @Override
-//     protected void doGet(HttpServletRequest request, HttpServletResponse response) 
-//             throws ServletException, IOException {
-//         response.setContentType("text/plain");
-//         response.setStatus(HttpServletResponse.SC_OK);
-//         response.getWriter().write("Hello from servlet");
-//     }
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+            throws ServletException, IOException {
+        response.setContentType("text/plain");
+        response.setStatus(HttpServletResponse.SC_OK);
+        response.getWriter().write("Hello from servlet");
+    }
 
-//     public String getMessage() {
-//         return message;
-//     }
-// }
+    public String getMessage() {
+        return message;
+    }
+}
